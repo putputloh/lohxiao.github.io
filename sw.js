79c5858f7d45a9152e54fbe9bb9857a9c8e5b00a
xiaoloh <putputloh@gmail.com>
2021-08-26 15:28:41 +0800

    Personal Webpage Updated

--- sw.js
+++ sw.js
@@ -3,7 +3,18 @@ var filesToCache = [
     '/',
     '/index.html',
     '/css/style.css',
-    '/js/main.js'
+    '/js/main.js',
+    '/img/Background.png',
+    '/img/Loh Xiao.jpg',
+    '/img/whatsapp.png',
+    '/img/icons/icon1.png',
+    '/img/icons/icon2.png',
+    '/img/icons/icon3.png',
+    '/img/icons/icon4.png',
+    '/img/icons/icon5.png',
+    '/img/icons/icon6.png',
+    '/img/icons/icon7.png',
+    '/img/icons/icon8.png'
 ];
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function (e) {
